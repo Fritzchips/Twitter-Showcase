ab2ed429646c1aee12e577f18ce22f0845dbbf30
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-07-02 09:13:19 -0700

    added Procfile

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const axios = require("axios");
 require("dotenv").config();
 
@@ -177,4 +177,4 @@ app.get("*", (req, res) => {
   res.status(404).send("Page Not Found");
 });
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
